c5ce9b243d31dc8655efeda1b061d3b93b2f0372
jaber-tebyan <jabertebyan@gmail.com>
2026-03-16 10:29:57 +0000

    feat: add CategoriesController

--- Data/Migrations/20260316102550_mm2.Designer.cs
+++ Data/Migrations/20260316102550_mm2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using lab1.Data;
 
@@ -10,9 +11,11 @@ using lab1.Data;
 namespace lab1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260316102550_mm2")]
+    partial class mm2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.12");
